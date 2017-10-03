fd3fb56b3ff2c22f21e92096646c6710542ae0d0
mktcode <kontakt@markus-kottlaender.de>
2017-10-03 16:12:57 +0200

    added rpc client

---
+++ src/AppBundle/Service/SteemClient.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mkt
+ * Date: 28.09.17
+ * Time: 12:58
+ */
+
+namespace AppBundle\Service;
+
+use Doctrine\ORM\EntityManager;
+
+class SteemClient
+{
+    /**
+     * @var EntityManager
+     */
+    private $em;
+
+    function __construct(EntityManager $em) {
+
+        $this->em = $em;
+    }
+
+    public function getIncomingVotes($author) {
+        $sql = "SELECT t.* FROM sbds.sbds_tx_votes t WHERE t.author = :author";
+        $params = ['author' => $author];
+
+        $stmt = $this->em->getConnection()->prepare($sql);
+        $stmt->execute($params);
+        return $stmt->fetchAll();
+    }
+
+    public function getMentions($author) {
+        $sql = "SELECT c.* FROM sbds.sbds_tx_comments c WHERE c.body LIKE :author OR c.title LIKE :author";
+        $params = ['author' => '%@' . $author . '%'];
+
+        $stmt = $this->em->getConnection()->prepare($sql);
+        $stmt->execute($params);
+        return $stmt->fetchAll();
+    }
+}
